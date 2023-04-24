6e7078a4ea173a4a121d93136f2ee2d38e5d5041
RazeHazee <caymanisland1010@gmial.com>
2023-04-24 13:26:34 -0500

    check() fixing

--- R/plot.Rttest.R
+++ R/plot.Rttest.R
@@ -1,7 +1,7 @@
 
 #' plot.Rttest
 #'
-#' @param w output of myttest function
+#' @param x output of myttest function
 #' @param ... additional parameters
 #'
 #' @return ggplot2 boxplot of the dataframe, differs depending on t.test used
@@ -9,8 +9,8 @@
 #' @export
 #'
 #' @examples set.seed(20); plot(myttest(rnorm(30,5,2), rnorm(30,3,2), .05, FALSE))
-plot.Rttest = function(w, ...) {
-  ggplot(data = w$data.frame, aes(x = w$data.frame$df.x, y = w$data.frame$df.y)) +
+plot.Rttest = function(x, ...) {
+  ggplot(data = x$data.frame, aes(x = x$data.frame$df.x, y = x$data.frame$df.y)) +
     geom_boxplot()
 }
 
--- R/print.Rttest.R
+++ R/print.Rttest.R
@@ -2,7 +2,8 @@
 #' print.Rttest
 #' defines the print method for the Rttest class
 #'
-#' @param list #the output of the myttest function
+#' @param x #the output of the myttest function
+#' @param ... add. parameters
 #'
 #'
 #' @return returns a list of length 6: kable table of x and y
@@ -10,18 +11,17 @@
 #' p.value of the t.test, a rejection statement,
 #' and the alpha value used.
 #' @import kableExtra
-#'
+#' @method print Rttest
 #' @export
 #'
 #' @examples set.seed(20); print(myttest(rnorm(30,5,2), rnorm(30,3,2), .05, FALSE))
-print.Rttest = function(list) {
-  print_output = list(kable.table = kable(list$data.frame),
-                      confidence.interval = list$confidence.interval,
-                      test_type = list$test_type,
-                      p.value = list$p.value,
-                      alpha = list$alpha,
-                      reject = list$reject)
+print.Rttest = function(x, ...) {
+  print_output = list(kable.table = kable(x$data.frame),
+                      confidence.interval = x$confidence.interval,
+                      test_type = x$test_type,
+                      p.value = x$p.value,
+                      alpha = x$alpha,
+                      reject = x$reject)
   print_output
-  UseMethod("print.Rttest")
 }
 
