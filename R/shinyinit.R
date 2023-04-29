ae2612cb16902d21e58a226669b4760a86f92279
RazeHazee <caymanisland1010@gmial.com>
2023-04-28 21:23:12 -0500

    vin and shiny

---
+++ R/shinyinit.R
@@ -0,0 +1,18 @@
+
+
+#' shinyinit
+#'  @description
+#'  Starts an example shiny server in the browser. This is designed to show off
+#'  some of the functionaltiy of the plot and print methods o
+#'
+#'
+#' @return starts the shiny example for mypackage
+#' @export
+#'
+#' @examples
+#' \dontrun{shinyinit()}
+shinyinit() = function()
+{
+  shiny::runApp(system.file("shiny-1", package =
+                            "mypackage"), launch.browser = TRUE)
+}
